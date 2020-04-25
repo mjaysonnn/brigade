4cd54a227e77b4bd3719a2b5c1dd406dc81f55a0
jashwantraj92 <jashwant.raj92@gmail.com>
2020-04-25 18:35:43 +0000

    parse_logs.py file modie=fied to print start and end time od pods. pod_list file passed as command line argument

--- parse_logs.py
+++ parse_logs.py
@@ -1,6 +1,26 @@
 import subprocess
+import sys,os
 import json
-output = subprocess.check_output("kubectl get pod imc-01e6czwbz42wvs9h0myknfczgj -o json", shell=True)
-y = json.loads(output)
-print y
+workload = open(sys.argv[1], 'r')
+line = workload.readline()
+with open(sys.argv[1]) as f:
+    alist = [line.rstrip() for line in f]
+print alist
+
+for line in alist:
+    cmd = "kubectl get pod %s -o json"%(line)
+    print(line, cmd)
+    output = subprocess.check_output(cmd, shell=True)
+    y = json.loads(output)
+    print y['status']['initContainerStatuses'][0]['state']['terminated']['startedAt']
+    print y['status']['initContainerStatuses'][0]['state']['terminated']['finishedAt']
+    finishTime =  y['status']['conditions'][3]['lastTransitionTime']
+    startTime =  y['status']['conditions'][0]['lastTransitionTime']
+    cmd="date -d %s "%(finishTime)
+    cmd = cmd+"+%s"
+    finishTimestamp = os.system(cmd);
+    cmd="date -d %s "%(startTime)
+    cmd = cmd+"+%s"
+    startTimestamp = os.system(cmd);
+    print(startTimestamp, finishTimestamp)
 
